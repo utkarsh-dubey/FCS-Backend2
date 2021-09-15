9e212bf082c140799d2eedce604e6712fa74daf0
robin828 <61390860+robin828@users.noreply.github.com>
2021-09-15 22:06:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-
-var speakeasy = require("speakeasy");
-const secret = speakeasy.generateSecret({length: 20});
 const express = require('express')
 const app = express()
-var QRCode = require('qrcode');
 const port = 3000
-
-
-QRCode.toDataURL(secret.otpauth_url, function(err, data_url) {
-    console.log(data_url);
-
-})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`, secret);
+  console.log(`Example app listening at http://localhost:${port}`);
 })
