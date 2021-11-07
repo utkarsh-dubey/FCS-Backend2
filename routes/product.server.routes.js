456eebc8ad9e971ab02ba9fcc8084af0308e1492
Utkarsh dubey <dubeyutkarsh2000@gmail.com>
2021-11-07 21:06:47 +0530

    send user

--- routes/product.server.routes.js
+++ routes/product.server.routes.js
@@ -7,6 +7,24 @@ const User = require('../models/user');
 const Product = require('../models/product');
 
 
+productRouter.get("/",(req,res) =>{
+
+    Product.find({}).exec((err,products) => {
+
+        if(err){
+            return res.status(400).send({message:"some error occured in db"});
+        }
+
+        return res.status(200).send(products);
+
+    });
+
+});
+
+productRouter.get('/:id',(req,res) => {
+    
+});
+
 
 
 
