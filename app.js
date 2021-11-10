5f6e6d4e209ceaa0b30b4022b2d2c53649ae63ce
Robin Garg <64920972+Robin-garg23@users.noreply.github.com>
2021-11-10 20:37:56 +0530

    auth

--- app.js
+++ app.js
@@ -36,8 +36,9 @@ const limiter = rateLimit({
 
 app.use((req, res, next)=>{
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Headers', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next()
 })
 // app.use(upload.array()); 
