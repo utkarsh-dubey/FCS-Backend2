1189e785d021bc7a2a0bdf11b6ec04281ff8574e
Utkarsh dubey <dubeyutkarsh2000@gmail.com>
2022-09-12 17:57:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ const limiter = rateLimit({
 });
 
 app.use((req, res, next)=>{
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', '"Origin, X-Requested-With, Authorization"');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE');
   // res.setHeader('Access-Control-Allow-Credentials', 'true');
